f07536d68f6f408ea6fba62bfb55620e96b510be
AyoubNas <nasraddine.ayoub@gmail.com>
2016-03-30 17:36:36 +0200

    first commit, project tree

---
+++ src/graph/AdjMatrixGraph.java
@@ -0,0 +1,5 @@
+import java.util.*;
+package graph;
+public class AdjMatrixGraph implements Graph{
+private LinkedList<LinkedList<Vertex>> adjacencyList;
+}
---
+++ src/graph/TestImpl.java
@@ -0,0 +1,1 @@
+package graph;private class TestImpl {Graph g = new AdjMatrixGraph(); Vertex v = new Vertex();g.addVertex(v);}
