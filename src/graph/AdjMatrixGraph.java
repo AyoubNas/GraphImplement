1d8703efa857a319005c90006163972d13cedda3
AyoubNas <nasraddine.ayoub@gmail.com>
2016-03-30 22:37:18 +0200

    minor

--- src/graph/AdjMatrixGraph.java
+++ src/graph/AdjMatrixGraph.java
@@ -4,11 +4,17 @@ package graph;
 public class AdjMatrixGraph implements Graph{
 	private LinkedList<LinkedList<Vertex>> adjacencyList= new LinkedList();
 
-	private void addVertex(Vertex v){
+    private void addVertex(Vertex v){
 		LinkedList<Vertex> l = new LinkedList();
 		l.add(v);
 		adjacencyList.add(l);
 	}
+
+    public void  addEdge(Edge e){
+        Vertex v1=e.getExtremites()[0];
+        Vertex v1=e.getExtremites()[1];
+        
+    }  
 
 	
 }
