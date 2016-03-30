5a11009f803cba2a0323832ed00d9ba915110ccc
AyoubNas <nasraddine.ayoub@gmail.com>
2016-03-30 18:26:45 +0200

    added function addVertex

--- src/graph/AdjMatrixGraph.java
+++ src/graph/AdjMatrixGraph.java
@@ -2,5 +2,13 @@ import java.util.*;
 package graph;
 
 public class AdjMatrixGraph implements Graph{
-	private LinkedList<LinkedList<Vertex>> adjacencyList;
+	private LinkedList<LinkedList<Vertex>> adjacencyList= new LinkedList();
+
+	private void addVertex(Vertex v){
+		LinkedList<Vertex> l = new LinkedList();
+		l.add(v);
+		adjacencyList.add(l);
+	}
+
+	
 }
